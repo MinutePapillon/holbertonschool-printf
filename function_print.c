bde9567c0398fddb6c0b4fc8241dfda8b0ed1eff
MinutePapillon <christophebarrere@gmail.com>
2025-03-25 11:47:54 +0100

    First version function_print

---
+++ function_print.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+ * print_percent - prints an %
+ * @ap: the list of ap
+ */
+int print_percent(va_list ap)
+{
+	_putchar('%');
+	return(1);
+}
+/**
+ * print_char - prints a char
+ * @ap: the list of ap
+ */
+int print_char(va_list ap)
+{
+	char c = va_arg(ap, int);
+	_putchar(c)a;
+	return(1);
+}
+/**
+ * print_string - prints a string
+ * @ap: the list of ap
+ */
+int print_string(va_list ap)
+{
+	int i = 0;
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return(6);
+	}
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return(i);
+}
+}
