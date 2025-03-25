d6b569a107884aab4e83199b058841b0dbac5101
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-25 12:56:55 +0100

    did the specifier file and the printf_function files but christophe said he already did it

---
+++ print_f_functions.c
@@ -0,0 +1,8 @@
+#include "printf.h"
+/**
+ *
+ *
+ *
+ *
+ */
+print_char(
---
+++ specif.c
@@ -0,0 +1,27 @@
+#include "prinf.h"
+/**
+ *
+ *
+ *
+ */
+int specif(const char character, va_list args)
+{
+	int j = 0;
+
+	spec_t specifier[] = {
+		{'%', print_percent},
+		{'s', print_string},
+		{'c', print_char},
+		{'\0', NULL},
+	};
+
+	while (specifier[j].specifiers)
+	{
+		if (specifier[j].specifiers = character)
+		{
+			return (specifier[j].function(args));
+		}
+		j++;
+	}
+	return (-1);
+}
