e93fccc6e7e14683cc1ae3548be6ecbb8fbc2fd5
MinutePapillon <christophebarrere@gmail.com>
2025-03-25 18:53:56 +0100

    Add comments

--- main.h
+++ main.h
@@ -3,15 +3,19 @@
 #include <stdarg.h>
 #include <stddef.h>
 
+/**
+ * struct spec - Structure defining a format specifier
+ * @s: Character representing the specifier ('c' for char, 's' for string)
+ * @f: Pointer to the function that handles this specifier
+ */
 typedef struct spec
 {
 	char s;
 	int (*f)(va_list);
-}spec_t;
+} spec_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_percent();
 int print_string(va_list ap);
 int print_char(va_list ap);
 int specif(const char character, va_list args);
