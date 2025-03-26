06abca1a842d83207dc37c0dfa5ed4e7e8462c79
MinutePapillon <christophebarrere@gmail.com>
2025-03-26 11:22:44 +0100

    Add function print_int

--- function_print.c
+++ function_print.c
@@ -1,8 +1,44 @@
 #include <stdarg.h>
 #include "main.h"
 /**
+ * print_int - print a int
+ * @ap: the list of ap
+ *
+ * Return: number of character printed
+ */
+int print_int(va_list ap)
+{
+	int i = va_arg(ap, int), k = 0, l = 0, t = 0;
+	int j = i;
+	int counter = 0;
+
+	if (i < 0)
+	{
+		_putchar('-');
+		t++;
+	}
+
+	while (j > 10)
+	{
+		j /= 10;
+		counter++;
+	}
+
+	while (counter > 0)
+	{
+		k = i / (10 * counter);
+		_putchar(k + '0');
+		counter--;
+	}
+	l = i % 10;
+	_putchar(l + '0');
+	return (counter + 1 + t);
+}
+/**
  * print_char - prints a char
  * @ap: the list of ap
+ *
+ * Return: number of character printed
  */
 int print_char(va_list ap)
 {
@@ -14,6 +50,8 @@ int print_char(va_list ap)
 /**
  * print_string - prints a string
  * @ap: the list of ap
+ *
+ * Return: number of character printed
  */
 int print_string(va_list ap)
 {
